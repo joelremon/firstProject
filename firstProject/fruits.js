1164882e3d19d2aa1b8f9fd9cfa0aadd6d01903e
joelremon <joeaugus11@gmail.com>
2024-01-09 21:48:36 +0530

    first commit

---
+++ firstProject/fruits.js
@@ -0,0 +1,10 @@
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color = 'orange';
+const headingEdit = document.getElementById('header');
+headingEdit.style.backgroundColor = 'green';
+headingEdit.style.borderBottom = '5px solid orange';
+const secondHeading = document.getElementById('basket-heading');
+secondHeading.style.color = 'green';
+const exit = document.getElementById('thanks');
+exit.innerHTML = '<p>Please visit us again</p>'
