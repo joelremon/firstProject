00aa876f6504541440b15e1ce6e10ac24cc0ed19
joelremon <joeaugus11@gmail.com>
2024-01-09 22:31:25 +0530

    Applied querySelector method

--- firstProject/fruits.js
+++ firstProject/fruits.js
@@ -1,22 +1,41 @@
-const mainHeading = document.getElementById('main-heading');
-mainHeading.textContent = 'Fruit World';
-mainHeading.style.color = 'orange';
-const headingEdit = document.getElementById('header');
-headingEdit.style.backgroundColor = 'green';
-headingEdit.style.borderBottom = '5px solid orange';
-const secondHeading = document.getElementById('basket-heading');
-secondHeading.style.color = 'green';
-const exit = document.getElementById('thanks');
-exit.innerHTML = '<p>Please visit us again</p>';
+const fruitsMod = document.querySelector('.fruits');
+fruitsMod.style.backgroundColor = 'gray';
+fruitsMod.style.padding = '30px';
+fruitsMod.style.margin = '30px';
+fruitsMod.style.width = '50%';
+fruitsMod.style.borderRadius = '5px';
+fruitsMod.style.listStyleType = 'none';
 
-const fruits = document.getElementsByClassName('fruit');
-fruits[2].style.backgroundColor = "yellow";
-for(let i=0; i<fruits.length; i++) {
-  fruits[i].style.fontWeight = 'bold';
+const mainHeading = document.querySelector('#main-heading');
+mainHeading.style.textAlign = 'right';
+
+const fruits = document.querySelector('.fruits');
+fruits.style.backgroundColor = 'gray';
+
+const basketHead = document.querySelector('h2');
+basketHead.style.marginLeft = '30px';
+
+
+
+ const fruitItems = document.querySelectorAll('.fruit');
+ for(let i=0; i<fruitItems.length; i++) {
+ fruitItems[i].style.backgroundColor = 'lightgray';
+ fruitItems[i].style.padding = '10px';
+ fruitItems[i].style.margin = '10px'; 
+ fruitItems[i].style.borderRadius = '5px';
+ fruitItems[i].style.listStyleType = 'none';
+ }
+
+ 
+const basketHeading = document.querySelector('#basket-heading');
+basketHeading.style.color = 'brown';
+
+const fruit = document.querySelectorAll('.fruit:nth-child(even)');
+for(let i=0; i<fruit.length; i++) {
+  fruit[i].style.backgroundColor = 'brown';
 }
 
-const listItems = document.getElementsByTagName('li');
-listItems[4].style.color = "blue";
-for(let i=0; i<listItems.length; i++) {
-  listItems[i].style.fontStyle = 'italic';
+const oddTextColor = document.querySelectorAll('.fruit:nth-child(even)');
+for(let i=0; i<oddTextColor.length; i++) {
+  oddTextColor[i].style.color = 'white';
 }
