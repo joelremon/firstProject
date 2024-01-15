554a71c37f4b5f9f658b598bc553c9af097dffb7
joelremon <joeaugus11@gmail.com>
2024-01-15 23:07:34 +0530

    Added comments

--- firstProject/bookingApp/booking.js
+++ firstProject/bookingApp/booking.js
@@ -10,13 +10,14 @@ function handleFormSubmit(event) {
     email,
     phone
   };
-
+  //Email address as key
   localStorage.setItem(userDetails.email,JSON.stringify(userDetails));
   addListItem(userDetails);
 }
 
 function addListItem(userDetails) {
   const ul = document.getElementById('listOfItems');
+  //Create new list item
   const li = document.createElement('li');
   const liText = document.createTextNode(userDetails.username+' - '+userDetails.email+' - '+userDetails.phone);
   li.appendChild(liText);
